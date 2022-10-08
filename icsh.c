65311582e04349035a15a37d26ec96d22aa691ef
yuttasartviratpan <yuttasart.vir@student.mahidol.edu>
2022-10-09 00:46:28 +0700

    0.2.0 - Milestone 2: Completed

--- icsh.c
+++ icsh.c
@@ -3,10 +3,12 @@
  * StudentID: 6281370
  */
 
+#include <sys/wait.h>
 #include "stdio.h"
 #include "string.h"
 #include "stdlib.h"
 #include "ctype.h"
+#include "unistd.h"
 #define MAX_CMD_BUFFER 255
 
 /*
@@ -31,11 +33,14 @@ static const char * const command_name[] = {
 
 
 /* string_process()
- * Input: input[] = the whole string from the input buffer.
+ * Input: input[] = The whole string from the input buffer.
  *
  * Functionality: Trims all the white characters that comes before the first char.
  *      Ex: ____echo hello -> echo hello
  *          ____echo hi__ -> echo hi__
+ *
+ * Return/Output:
+ *      - A character pointer (string) that points to the trimmed string.
  */
 char* string_process(char input[]){
     int actual_string_length = 0;
@@ -47,11 +52,16 @@ char* string_process(char input[]){
     }
     int string_arr_size = 1;
     int string_arr_pos = 0;
+    int extra_newline = 0;
+    if (input[strlen(input)-1] != '\n'){ // In script, if End-Of-String doesn't contain newline, add it.
+        extra_newline = 1;
+    }
 
-    for (int pos = 0; (pos < strlen(input)-1); pos++){
+    for (int pos = 0; (pos < strlen(input)-1 + extra_newline); pos++){
         if (!isspace(input[pos]) && input[pos] != '\0' && met_first_char == 0){
             met_first_char = 1;
             actual_string_length++;
+
             if (actual_string_length <= string_arr_size){
                 string_arr[string_arr_pos] = input[pos];
                 string_arr_pos++;
@@ -62,9 +72,11 @@ char* string_process(char input[]){
                 string_arr[string_arr_pos] = input[pos];
                 string_arr_pos++;
             }
+
         }
         else if (!isspace(input[pos]) && input[pos] != '\0' && met_first_char == 1){
             actual_string_length++;
+
             if (actual_string_length <= string_arr_size){
                 string_arr[string_arr_pos] = input[pos];
                 string_arr_pos++;
@@ -75,9 +87,11 @@ char* string_process(char input[]){
                 string_arr[string_arr_pos] = input[pos];
                 string_arr_pos++;
             }
+
         }
         else if (isspace(input[pos]) && input[pos] != '\0' && met_first_char == 1){
             actual_string_length++;
+
             if (actual_string_length <= string_arr_size){
                 string_arr[string_arr_pos] = input[pos];
                 string_arr_pos++;
@@ -88,9 +102,9 @@ char* string_process(char input[]){
                 string_arr[string_arr_pos] = input[pos];
                 string_arr_pos++;
             }
+
         }
     }
-
 
     if (actual_string_length == 0){
         free(string_arr);
@@ -109,11 +123,13 @@ char* string_process(char input[]){
 
 /* command_executor()
  * Input:
- *      - command = the command part of the user inputted buffer.
- *      - argument = the argument part of the user inputted buffer.
- *      - prev_input[] = array of string, that stores previous command and argument.
+ *      - command = The command part of the user inputted buffer.
+ *      - argument = The argument part of the user inputted buffer.
+ *      - prev_input[] = Array of string, that stores previous command and argument.
  *              prev_input[0] = previous command.
  *              prev_input[1] = previous argument.
+ *      - oristr (Original String) = A pointer that holds the address of the trimmed string.
+ *              Used to free the memory allocation when exit system calls is called.
  *      - mode = Determinant to update prev_input[], used in double-bang (!!)
  *              0 = allowed updating of prev_input[], 1 otherwise.
  *
@@ -121,9 +137,12 @@ char* string_process(char input[]){
  *      - Checks "command", whether the command matches any recorded known command (in enum above).
  *      - If match recorded command, does the functionality it supposed to, otherwise output unknown command.
  *      - If the command requires argument, then it uses the argument as well.
+ *      - If exit ever reaches, free all the malloc-ed pointers.
  *
+ * Return/Output:
+ *      - Nothing, it's void
  */
-void command_executor(char* command, char* argument, char* prev_input[], int mode){
+void command_executor(char* command, char* argument, char* prev_input[], char* oristr, FILE* fileptr, int mode){
     if(strcmp(command, command_name[ECHO]) == 0){
         if(mode == 0){
             prev_input[0] = realloc(prev_input[0], strlen(command) * sizeof(char));
@@ -148,11 +167,11 @@ void command_executor(char* command, char* argument, char* prev_input[], int mod
         printf("%s\n", argument);
     }
     else if(strcmp(command, command_name[DOUBLE_BANG]) == 0){
-        if(*prev_input[0] == '\0'){
+        if(*prev_input[0] == '\0'){ // If there is no command, just skip.
             return;
         }
         printf("%s %s\n", prev_input[0], prev_input[1]);
-        command_executor(prev_input[0], prev_input[1], NULL, 1);
+        command_executor(prev_input[0], prev_input[1], NULL, oristr, fileptr, 1);
     }
     else if(strcmp(command, command_name[EXIT]) == 0){
         int status = atoi(argument);
@@ -161,6 +180,14 @@ void command_executor(char* command, char* argument, char* prev_input[], int mod
             status -= truncation * 256;
         }
         printf("Good bye\n");
+        free(command);
+        free(argument);
+        free(oristr);
+        free(prev_input[0]);
+        free(prev_input[1]);
+        if(fileptr != NULL){
+            fclose(fileptr);
+        }
         exit(status);
     }
     else{
@@ -172,10 +199,12 @@ void command_executor(char* command, char* argument, char* prev_input[], int mod
 
 /* command_process_unit()
  * Input:
- *      - input = the whole string from the input buffer.
- *      - prev_input[] = array of string, that stores previous command and argument.
+ *      - input = The whole string from the input buffer.
+ *      - prev_input[] = Array of string, that stores previous command and argument.
  *              prev_input[0] = previous command.
  *              prev_input[1] = previous argument.
+ *      - fileptr = A file pointer, used to close the file to free the memory allocation
+ *              when exit system calls is called.
  *
  * Functionality:
  *      - Trims the inputted string by calling string_process()
@@ -183,10 +212,12 @@ void command_executor(char* command, char* argument, char* prev_input[], int mod
  *      - It then runs command_executor(), passing in the extracted command, arguments and prev_input[] in.
  *      - command_executor() ran in this way is set to mode = 0.
  *      - If "input" is null-terminated after trimming the string, the whole function is returned.
+ *      - Free the related pointers after a command is done executing, unless <exit> is called.
  *
- *
+ * Return/Output:
+ *      - Nothing, it's void
  */
-void command_process_unit(char input[], char* prev_input[]){
+void command_process_unit(char input[], char* prev_input[], FILE* fileptr){
     char* trim_input = string_process(input);
     if (trim_input[0] == '\0'){
         free(trim_input);
@@ -251,7 +282,6 @@ void command_process_unit(char input[], char* prev_input[]){
             printf("argument memory allocation failed");
             exit(1);
         }
-
         command[command_pos] = '\0';
         arguments[argument_pos] = '\0';
     }
@@ -261,12 +291,11 @@ void command_process_unit(char input[], char* prev_input[]){
             printf("command memory allocation failed");
             exit(1);
         }
-
         command[command_pos] = '\0';
         arguments[0] = '\0';
     }
 
-    command_executor(command, arguments,  prev_input, 0);
+    command_executor(command, arguments,  prev_input, trim_input, fileptr, 0);
     free(command);
     free(arguments);
     free(trim_input);
@@ -274,26 +303,74 @@ void command_process_unit(char input[], char* prev_input[]){
 }
 
 
+
 /* main()
- * Just a placeholder where everything comes together, nothing exciting for now.
+ * Input:
+ *      - argc (aka. Argument Count) = Counting the number of arguments when executing program on the terminal
+ *      - argv[] (aka. Argument Vector) = The stored string of argument when executing program with inputted
+ *              argument on the terminal
  *
- * prev_input[0] and prev_input[1] are malloc-ed with intent of remember the string after other
- *      function(s) edit them.
+ * Functionality:
+ *      - Be the main (protagonist) where all functions come together
+ *      - If there is no file input, wait for user to input a command and an argument in.
+ *      - If there is an argument given in a file script/input, attempt to read the file line-by-line, exit if can't
+ *          - Continue to wait for stdin intake, just like when there is no file input
+ *      - Process the input by running command_process_unit()
+ *      - Keep repeating until exit command is called.
+ *
+ * Return/Output:
+ *      - An integer that supposedly has no use (for now?)
+ *
+ * File reading: Rob. (2016, August 30). C read file line by line. StackOverFlow.
+ *      https://stackoverflow.com/questions/3501338/c-read-file-line-by-line
+ *
+ * (Build-up) forking and waiting: xor007. (2015, January 11). How to get the return value of a program ran
+ *      via calling a member of the exec family of functions?. StackOverFlow.
+ *      https://stackoverflow.com/questions/2667095/how-to-get-the-return-value-of-a-program-ran-via-calling-a-member-of-the-exec-fa
+ *
  *
  */
-int main() {
+int main(int argc, char* argv[]) {
     char buffer[MAX_CMD_BUFFER];
     char* prev_input[2];
     prev_input[0] = (char*) malloc(sizeof(char));
     prev_input[1] = (char*) malloc(sizeof(char));
     printf("Starting IC shell\n");
+    if (argc == 2){
+        int status;
+        pid_t pid = fork();
+
+        if (pid < 0){
+            printf("fork() failed\n");
+        }
+        else if (pid == 0){
+            // child process
+            FILE *fptr;
+            char file_name[3] = {'.', '/', '\0'};
+            strcat(file_name, argv[1]);
+            fptr = fopen(file_name,"r");
+            if(fptr == NULL){
+                printf("File not found\n");
+                exit(1);
+            }
+            while(fgets(buffer, MAX_CMD_BUFFER, fptr)) {
+                command_process_unit(buffer, prev_input, fptr);
+            }
+        }
+        else{
+            wait(&status);
+            if(WIFEXITED(status))
+                printf("Child's exit code is: %d\n", WEXITSTATUS(status));
+            else
+                printf("Child did not terminate with exit\n");
+        }
+    }
     while (1) {
         printf("icsh $ ");
         fgets(buffer, 255, stdin);
-        command_process_unit(buffer, prev_input);
+        command_process_unit(buffer, prev_input, NULL);
     }
-    free(prev_input[0]);
-    free(prev_input[1]);
-}
 
+    return 0;
+}
 
